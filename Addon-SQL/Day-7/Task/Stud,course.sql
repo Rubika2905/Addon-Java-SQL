db8b96fc4b0c5a456c492c53d1696159493c7ff4
Rubika2905 <rubika2906@gmail.com>
2025-06-19 14:59:51 +0530

    Create Stud,course.sql

---
+++ Addon-SQL/Day-7/Task/Stud,course.sql
@@ -0,0 +1,26 @@
+mysql> create table student(
+    -> sroll int,
+    -> sname varchar(50)
+    -> );
+Query OK, 0 rows affected (0.04 sec)
+mysql> select*from student;
++-------+-----------+
+| sroll | sname     |
++-------+-----------+
+|       |           |   
++-------+-----------+
+0 rows in set (0.00 sec)
+
+
+mysql> create table Course(
+    -> Course int
+    -> );
+
+mysql> select*
+    -> from course;
++-----------+
+| Course    |
++-----------+
+|           | 
++-----------+
+5 rows in set (0.00 sec)
