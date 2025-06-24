0129637b8c48953cc8ba1ebd0d22fe19cf6488ab
Rubika2905 <rubika2906@gmail.com>
2025-06-24 19:35:44 +0530

    Update MainPrgm.sql

--- Addon-SQL/Day-9/Task/MainPrgm.sql
+++ Addon-SQL/Day-9/Task/MainPrgm.sql
@@ -34,3 +34,44 @@ mysql> select*from Customers;
 |          5 | Mini  | mini01@gmail.com  | China       | 2025-01-01 |        640.90 |
 +------------+-------+-------------------+-------------+------------+---------------+
 5 rows in set (0.00 sec)
+
+mysql> select*from Customers;
++------------+-------+-------------------+-------------+------------+---------------+
+| CustomerID | Name  | Email             | Country     | SignupDate | TotalPurchase |
++------------+-------+-------------------+-------------+------------+---------------+
+|          1 | Rubi  | rubi@gmail.com    | USA         | 2023-01-10 |        250.75 |
+|          2 | Abi   | abi@gmail.com     | Canada      | 2022-11-23 |        150.00 |
+|          3 | Kavya | kavya@gmail.com   | USA         | 2024-05-05 |        520.40 |
+|          4 | Sharu | Sharu07@gmail.com | South Korea | 2023-03-18 |         75.00 |
+|          5 | Mini  | mini01@gmail.com  | China       | 2025-01-01 |        640.90 |
++------------+-------+-------------------+-------------+------------+---------------+
+5 rows in set (0.00 sec)
+
+mysql> SELECT * FROM Customers
+    -> WHERE Country = 'USA';
++------------+-------+-----------------+---------+------------+---------------+
+| CustomerID | Name  | Email           | Country | SignupDate | TotalPurchase |
++------------+-------+-----------------+---------+------------+---------------+
+|          1 | Rubi  | rubi@gmail.com  | USA     | 2023-01-10 |        250.75 |
+|          3 | Kavya | kavya@gmail.com | USA     | 2024-05-05 |        520.40 |
++------------+-------+-----------------+---------+------------+---------------+
+2 rows in set (0.01 sec)
+
+mysql> SELECT * FROM Customers
+    -> WHERE TotalPurchase > 300;
++------------+-------+------------------+---------+------------+---------------+
+| CustomerID | Name  | Email            | Country | SignupDate | TotalPurchase |
++------------+-------+------------------+---------+------------+---------------+
+|          3 | Kavya | kavya@gmail.com  | USA     | 2024-05-05 |        520.40 |
+|          5 | Mini  | mini01@gmail.com | China   | 2025-01-01 |        640.90 |
++------------+-------+------------------+---------+------------+---------------+
+2 rows in set (0.00 sec)
+
+mysql> SELECT * FROM Customers
+    -> WHERE Country = 'USA' AND TotalPurchase > 300;
++------------+-------+-----------------+---------+------------+---------------+
+| CustomerID | Name  | Email           | Country | SignupDate | TotalPurchase |
++------------+-------+-----------------+---------+------------+---------------+
+|          3 | Kavya | kavya@gmail.com | USA     | 2024-05-05 |        520.40 |
++------------+-------+-----------------+---------+------------+---------------+
+1 row in set (0.00 sec)
